6ef49d9ee859835c7fe858a9da3c52b02ed071f0
Hope Neveux <hopeneveux@g.harvard.edu>
2022-11-08 18:05:22 -0500

    Updated 14 functions (top-down) for dual numbers

--- src/bad_package/elementary_functions.py
+++ src/bad_package/elementary_functions.py
@@ -1,5 +1,6 @@
 # Defines and describes the behavior of overloaded operators on different data types within the package
 # Numpy handles scalar and vector input by default, returning a scalar float and element-wise vector of floats respectively
+# All dual number computations used a trace table with V0 = x.real,  DpV0 = x.dual
 
 import numpy as np
 from bad_forward_mode import DualNumber
@@ -12,18 +13,18 @@ inf = np.inf
 # OVERLOADING FUNCTIONS
 def exp(x):
     if isinstance(x, DualNumber):
-        raise NotImplementedError('Devs haven\'t gotten her yet')
+        return DualNumber(np.exp(x.real), x.dual * np.exp(x.real))
 
     elif isinstance(x, (int, float, np.array)):
         # Returns basic e^x computation
         return np.exp(x)
 
     else:
-        raise ValueError('Something went wrong in exp()')
+        raise TypeError('The exp() function can only handle single DualNumbers, ints, floats, or lists, numpy arrays of these types.')
 
 def ln(x):
     if isinstance(x, DualNumber):
-        raise NotImplementedError('Devs haven\t gotten here yet')
+        return DualNumber(np.log(x.real), x.dual / x.real)
 
     elif isinstance(x, (int, float, np.array)):
         # Returns natural log
@@ -34,44 +35,44 @@ def ln(x):
             raise ValueError('Natural log is defined only for values greater than or equal to 1.')
 
     else:
-        raise ValueError('Something went wrong in ln()')
+        raise TypeError('The ln() function can only handle single DualNumbers, ints, floats, or lists, numpy arrays of these types.')
 
 def log10(x):
     if isinstance(x, DualNumber):
-        raise NotImplementedError('Devs haven\t gotten here yet')
+        raise NotImplementedError('Devs hadn\'t gotten here yet')
 
     elif isinstance(x, (int, float, np.array)):
         # General base in math, but denoted with 10 for user clarity
         return np.log10(x)
 
     else:
-        raise ValueError('Something went wrong in log10()')
+        raise TypeError('The log10() function can only handle single DualNumbers, ints, floats, or lists, numpy arrays of these types.')
 
 def log2(x):
     if isinstance(x, DualNumber):
-        raise NotImplementedError('Devs haven\t gotten here yet')
+        raise NotImplementedError('Devs haven\'t gotten here yet')
     
     elif isinstance(x, (int, float, np.array)):
         # Often used by computer scientists for algorithmic efficiency calculations
         return np.log2(x)
     
     else:
-        raise ValueError('Something went wrong in log2()')
+        raise TypeError('The log2() function can only handle single DualNumbers, ints, floats, or lists, numpy arrays of these types.')
 
 def logBase(base, x):
     if isinstance(x, DualNumber):
-        raise NotImplementedError('Devs haven\t gotten here yet')
+        raise NotImplementedError('Devs haven\'t gotten here yet')
     
     elif isinstance(x, (int, float, np.array)):    
         # Use change of base formula with common 10 base for custom log base computation
         return (np.log10(x) / np.log10(base))
     
     else:
-        raise ValueError('Something went wrong in logBase()')    
+        raise TypeError('The logBase() function can only handle single DualNumbers, ints, floats, or lists, numpy arrays of these types.') 
 
 def sin(x):
     if isinstance(x, DualNumber):
-        raise NotImplementedError('Devs haven\t gotten here yet')
+        return DualNumber(np.sin(x.real), x.dual * np.cos(x.real))
     
     elif isinstance(x, (int, float, np.array)):    
         return np.sin(x)
@@ -81,27 +82,29 @@ def sin(x):
 
 def cos(x):
     if isinstance(x, DualNumber):
-        raise NotImplementedError('Devs haven\t gotten here yet')
-    
+        return DualNumber(np.cos(x.real), -1 * np.sin(x.real) * x.dual)
+
     elif isinstance(x, (int, float, np.array)):    
         return np.cos(x)
 
     else:
-        raise ValueError('Something went wrong in cos()')
+        raise TypeError('The cos() function can only handle single DualNumbers, ints, floats, or lists, numpy arrays of these types.')
 
 def tan(x):
     if isinstance(x, DualNumber):
-        raise NotImplementedError('Devs haven\t gotten here yet')
-    
+        # Using our implementation of sec to reuse the code
+        return DualNumber(np.tan(x.real), x.dual * sec(x.real) * sec(x.real))
+
     elif isinstance(x, (int, float, np.array)):    
         return np.tan(x)
 
     else:
-        raise ValueError('Something went wrong in tan()')
+        raise TypeError('The tan() function can only handle single DualNumbers, ints, floats, or lists, numpy arrays of these types.')
 
 def csc(x):
     if isinstance(x, DualNumber):
-        raise NotImplementedError('Devs haven\t gotten here yet')
+        # Using our implementation of csc and cot
+        return DualNumber(csc(x.real), -1 * csc(x.real) * cot(x.dual) * x.dual)
     
     elif isinstance(x, (int, float, np.array)):    
         if np.sin(x) != 0:
@@ -111,12 +114,13 @@ def csc(x):
             raise ValueError('Cosecant is not defined for the input value.')
 
     else:
-        raise ValueError('Something went wrong in csc()')
+        raise TypeError('The csc() function can only handle single DualNumbers, ints, floats, or lists, numpy arrays of these types.')
             
 
 def sec(x):
     if isinstance(x, DualNumber):
-        raise NotImplementedError('Devs haven\t gotten here yet')
+        # Using our implementations of sec and tan
+        return DualNumber(sec(x.real), sec(x.real) * tan(x.real) * x.dual)
     
     elif isinstance(x, (int, float, np.array)):    
         if np.cos(x) != 0:
@@ -126,12 +130,13 @@ def sec(x):
             raise ValueError('Secant is not defined for the input value')
 
     else:
-        raise ValueError('Something went wrong in sec()')
+        raise TypeError('The sec() function can only handle single DualNumbers, ints, floats, or lists, numpy arrays of these types.')
 
 def cot(x):
     if isinstance(x, DualNumber):
-        raise NotImplementedError('Devs haven\t gotten here yet')
-    
+        # Using our implementation of cot and csc
+        return DualNumber(cot(x.real), -1 * csc(x.real) * csc(x.real) * x.dual)
+
     elif isinstance(x, (int, float, np.array)):    
         if np.tan(x) != 0:
             return (1 / np.tan(x))
@@ -140,31 +145,33 @@ def cot(x):
             raise ValueError('Cotangent is not defined for the input value')
 
     else:
-        raise ValueError('Something went wrong in cot()')
+        raise TypeError('The cot() function can only handle single DualNumbers, ints, floats, or lists, numpy arrays of these types.')
 
 def sinh(x):
     if isinstance(x, DualNumber):
-        raise NotImplementedError('Devs haven\t gotten here yet')
+        # Using our implementations of sinh and cosh
+        return DualNumber(sinh(x.real), cosh(x.real) * x.dual)
     
     elif isinstance(x, (int, float, np.array)):    
         return np.sinh(x)
 
     else:
-        raise ValueError('Something went wrong in sinh()')
+        raise TypeError('The sinh() function can only handle single DualNumbers, ints, floats, or lists, numpy arrays of these types.')
 
 def cosh(x):
     if isinstance(x, DualNumber):
-        raise NotImplementedError('Devs haven\t gotten here yet')
+        # Using our implementation of cosh and sinh
+        return DualNumber(cosh(x.real), sinh(x.real) * x.dual)
     
     elif isinstance(x, (int, float, np.array)):    
         return np.cosh(x)
 
     else:
-        raise ValueError('Something went wrong in cosh()')
+        raise TypeError('The cosh() function can only handle single DualNumbers, ints, floats, or lists, numpy arrays of these types.')
 
 def tanh(x):
     if isinstance(x, DualNumber):
-        raise NotImplementedError('Devs haven\t gotten here yet')
+        raise NotImplementedError('Devs haven\'t gotten here yet')
     
     elif isinstance(x, (int, float, np.array)):    
         return np.tanh(x)
@@ -174,7 +181,8 @@ def tanh(x):
 
 def arcsin(x):
     if isinstance(x, DualNumber):
-        raise NotImplementedError('Devs haven\t gotten here yet')
+        # Using our implementation of arcsin and sqrt
+        return DualNumber(arcsin(x.real), (1 / sqrt(1 - x.real ** 2)) * x.dual)
     
     elif isinstance(x, (int, float, np.array)):    
         return np.arcsin(x)
@@ -184,7 +192,7 @@ def arcsin(x):
 
 def arccos(x):
     if isinstance(x, DualNumber):
-        raise NotImplementedError('Devs haven\t gotten here yet')
+        raise NotImplementedError('Devs haven\'t gotten here yet')
     
     elif isinstance(x, (int, float, np.array)):    
         return np.arccos(x)
@@ -194,7 +202,7 @@ def arccos(x):
 
 def arctan(x):
     if isinstance(x, DualNumber):
-        raise NotImplementedError('Devs haven\t gotten here yet')
+        raise NotImplementedError('Devs haven\'t gotten here yet')
     
     elif isinstance(x, (int, float, np.array)):    
         return np.arctan(x)
@@ -204,7 +212,7 @@ def arctan(x):
 
 def arcsinh(x):
     if isinstance(x, DualNumber):
-        raise NotImplementedError('Devs haven\t gotten here yet')
+        raise NotImplementedError('Devs haven\'t gotten here yet')
     
     elif isinstance(x, (int, float, np.array)):    
         return np.arcsinh(x)
@@ -214,7 +222,7 @@ def arcsinh(x):
 
 def arccosh(x):
     if isinstance(x, DualNumber):
-        raise NotImplementedError('Devs haven\t gotten here yet')
+        raise NotImplementedError('Devs haven\'t gotten here yet')
     
     elif isinstance(x, (int, float, np.array)):    
         return np.arccosh(x)
@@ -224,7 +232,7 @@ def arccosh(x):
 
 def arctanh(x):
     if isinstance(x, DualNumber):
-        raise NotImplementedError('Devs haven\t gotten here yet')
+        raise NotImplementedError('Devs haven\'t gotten here yet')
     
     elif isinstance(x, (int, float, np.array)):    
         return np.arctanh(x)
@@ -234,7 +242,7 @@ def arctanh(x):
 
 def sqrt(x):
     if isinstance(x, DualNumber):
-        raise NotImplementedError('Devs haven\t gotten here yet')
+        raise NotImplementedError('Devs haven\'t gotten here yet')
     
     elif isinstance(x, (int, float, np.array)):    
         return np.sqrt(x)
