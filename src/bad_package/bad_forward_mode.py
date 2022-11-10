fba667e2c40f7f12ce924b12098fe90e49d4a83f
Danhee Kim <sharonkim@g.harvard.edu>
2022-11-10 10:27:31 -0500

    beginning to implement pow

--- src/bad_package/bad_forward_mode.py
+++ src/bad_package/bad_forward_mode.py
@@ -3,85 +3,89 @@ This module defines dunder methods to overload Python built-in operators in the 
 '''
 
 class DualNumber:
-    
-    _supported_scalars = (int,float)
-    
-    def __init__(self,real,dual=1.0):
+
+    _supported_scalars = (int, float)
+
+    def __init__(self, real, dual=1.0):
             self.real = real
             self.dual = dual
-    
-    def __add__(self,other):
-        if not isinstance(other,(*self._supported_scalars, DualNumber)):
+
+    def __add__(self, other):
+        if not isinstance(other, (*self._supported_scalars, DualNumber)):
             raise TypeError("Type not supported: must be int or float")
-        if isinstance(other,self._supported_scalars):
-            return DualNumber(other+self.real,self.dual)
+        if isinstance(other, self._supported_scalars):
+            return DualNumber(other+self.real, self.dual)
         else:
-            return DualNumber(self.real+other.real,self.dual+other.dual)
-        
-    def __radd__(self,other):
+            return DualNumber(self.real+other.real, self.dual+other.dual)
+
+    def __radd__(self, other):
         return self.__add__(other)
-    
-    def __sub__(self,other):
-        if not isinstance(other,(*self._supported_scalars, DualNumber)):
+
+    def __sub__(self, other):
+        if not isinstance(other, (*self._supported_scalars, DualNumber)):
             raise TypeError("Type not supported: must be int or float")
-        if isinstance(other,self._supported_scalars):
-            return DualNumber(self.real-other,self.dual)
+        if isinstance(other, self._supported_scalars):
+            return DualNumber(self.real-other, self.dual)
         else:
-            return DualNumber(self.real-other.real,self.dual-other.dual)
-    
-    def __rsub__(self,other):
-        if not isinstance(other,(*self._supported_scalars, DualNumber)):
+            return DualNumber(self.real-other.real, self.dual-other.dual)
+
+    def __rsub__(self, other):
+        if not isinstance(other, (*self._supported_scalars, DualNumber)):
             raise TypeError("Type not supported: must be int or float")
-        if isinstance(other,self._supported_scalars):
-            return DualNumber(other-self.real,-self.dual)
+        if isinstance(other, self._supported_scalars):
+            return DualNumber(other-self.real, -self.dual)
         else:
-            return DualNumber(-self.real+other.real,-self.dual+other.dual)
-    def __mul__(self,other):
-        if not isinstance(other,(*self._supported_scalars, DualNumber)):
-            raise TypeError("Type not supported: must be int or float") 
-        if isinstance(other,self._supported_scalars):
-            return DualNumber(self.real*other.real,self.dual*other.real)
+            return DualNumber(-self.real+other.real, -self.dual+other.dual)
+    def __mul__(self, other):
+        if not isinstance(other, (*self._supported_scalars, DualNumber)):
+            raise TypeError("Type not supported: must be int or float")
+        if isinstance(other, self._supported_scalars):
+            return DualNumber(self.real*other.real, self.dual*other.real)
         else:
-            return DualNumber(self.real*other.real,self.real*other.dual+other.real*self.dual)
-        
-    def __rmul__(self,other):
+            return DualNumber(self.real*other.real, self.real*other.dual+other.real*self.dual)
+
+    def __rmul__(self, other):
         return self.__mul__(other)
-    
-    def __truediv__(self,other):
-        if not isinstance(other,(*self._supported_scalars, DualNumber)):
-            raise TypeError("Type not supported: must be int or float") 
-        if isinstance(other,self._supported_scalars):
-            return DualNumber(self.real/other.real,self.dual/other.real)
+
+    def __truediv__(self, other):
+        if not isinstance(other, (*self._supported_scalars, DualNumber)):
+            raise TypeError("Type not supported: must be int or float")
+        if isinstance(other, self._supported_scalars):
+            return DualNumber(self.real/other.real, self.dual/other.real)
         else:
-            return DualNumber(self.real/other.real,(other.real*self.dual - self.real*other.dual)/(other.real*other.real))
-        
-    def __rtruediv__(self,other):
-        if not isinstance(other,(*self._supported_scalars, DualNumber)):
-            raise TypeError("Type not supported: must be int or float") 
-        if isinstance(other,self._supported_scalars):
-            return DualNumber(other.real/self.real,(-other.real*self.dual)/(self.real*self.real))
+            return DualNumber(self.real/other.real, (other.real*self.dual - self.real*other.dual)/(other.real*other.real))
+
+    def __rtruediv__(self, other):
+        if not isinstance(other, (*self._supported_scalars, DualNumber)):
+            raise TypeError("Type not supported: must be int or float")
+        if isinstance(other, self._supported_scalars):
+            return DualNumber(other.real/self.real, (-other.real*self.dual)/(self.real*self.real))
         else:
-            return DualNumber(other.real/self.real,-(other.real*self.dual - self.real*other.dual)/(other.dual*other.dual))
-        
+            return DualNumber(other.real/self.real, -(other.real*self.dual - self.real*other.dual)/(other.dual*other.dual))
+
     def __neg__(self):
-        if not isinstance(self,(*self._supported_scalars, DualNumber)):
-            raise TypeError("Type not supported: must be int or float") 
-        if isinstance(self,self._supported_scalars):
-            return DualNumber(self.real*(-1),self.dual*(-1))
+        if not isinstance(self, (*self._supported_scalars, DualNumber)):
+            raise TypeError("Type not supported: must be int or float")
+        if isinstance(self, self._supported_scalars):
+            return DualNumber(self.real*(-1), self.dual*(-1))
         else:
-            return DualNumber(self.real*(-1),self.dual*(-1))
-        
-    def __pow__(self,other):
+            return DualNumber(self.real*(-1), self.dual*(-1))
+
+    def __pow__(self, other):
+        if not isinstance(self, (*self._supported_scalars, DualNumber)):
+            raise TypeError("Type not supported: must be int or float")
+        if isinstance(other, self._supported_scalars):
+            return DualNumber(self.real ** other, self.dual)
+        else:
+            raise NotImplementedError
+
+    def __rpow__(self, other):
         raise NotImplementedError
-    
-    def __rpow__(self,other):
-        raise NotImplementedError
-        
-        
-        
+
+
 if __name__ == "__main__":
     # rudimentary test code
-    
+
     import numpy as np
     x1 = 0.5
     z = DualNumber(x1)
@@ -104,11 +108,9 @@ if __name__ == "__main__":
     f8 = 2/z
     # neg
     f9 = -z
-   
+
     f = np.array([f1, f2, f3, f4, f5, f6, f7, f8, f9])
     values = ["add", "radd", "sub", "rsub", "mul", "rmul", "truediv", "rtruediv", "neg"]
 
     for i in range(9):
         print(f' {values[i]}: real = {f[i].real} dual = {f[i].dual}')
-    
-    
