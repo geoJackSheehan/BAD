440825e1680187df85ee07ae442bb1481dc2620f
Hope Neveux <hopeneveux@g.harvard.edu>
2022-11-19 16:42:57 -0500

    Sep tests for traceback, fixed ln deriv assert

--- tests/test_derivs.py
+++ tests/test_derivs.py
@@ -7,21 +7,23 @@ from bad_package.fad.fad import DualNumber
 
 class TestDerivs():
 
-    def test_scalarDeriv(self):
+    def test_scalarExp(self):
         # Compute exp(x^2) deriv evaluated at x = 2
         # Deriv should be 4 * exp(4)
         x1 = DualNumber(2)
         result = exp(x1**2)
         assert pytest.approx(4 * np.exp(4)) == result.dual
 
+    def test_scalarSin(self):
         # Compute deriv of sin(2x) + 3 evaluated at x = pi
         # Deriv should be cos(2x) * 2 == 2 * cos(2pi) == 2
         x2 = DualNumber(pi)
         result2 = sin(2*x2) + 3
         assert pytest.approx(2 * np.cos(2*np.pi)) == result.dual
 
+    def test_scalarLn(self):
         # Compute deriv of ln(2x^3) evaluated at x = 4
-        # Deriv should be 3/x (simplified) == 3/4 == 0.75
+        # Deriv should be x/3 (simplified) == 4/3
         x3 = DualNumber(4)
         result3 = ln(2*x3**3)
-        assert pytest.approx(3 / 4) == result.dual
+        assert pytest.approx(4/3) == result.dual
