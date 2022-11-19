bcb8b17acb4d72b0ac016afa49d4d1984052c0bb
Hope Neveux <hopeneveux@g.harvard.edu>
2022-11-19 16:32:33 -0500

    Couple other complex scalar derivs

--- tests/test_derivs.py
+++ tests/test_derivs.py
@@ -8,10 +8,20 @@ from bad_package.fad.fad import DualNumber
 class TestDerivs():
 
     def test_scalarDeriv(self):
-        x1 = DualNumber(1)
+        # Compute exp(x^2) deriv evaluated at x = 2
+        # Deriv should be 4 * exp(4)
+        x1 = DualNumber(2)
         result = exp(x1**2)
+        assert pytest.approx(2 * np.e) == result.dual
 
-        assert pytest.approx(2 * e) == result.dual
+        # Compute deriv of sin(2x) + 3 evaluated at x = pi
+        # Deriv should be cos(2x) * 2 == 2 * cos(2pi) == 2
+        x2 = DualNumber(pi)
+        result2 = sin(2*x2) + 3
+        assert pytest.approx(2 * np.cos(2*x2)) == result.dual
 
-
-        
+        # Compute deriv of ln(2x^3) evaluated at x = 4
+        # Deriv should be 3/x (simplified) == 3/4 == 0.75
+        x3 = DualNumber(4)
+        result3 = ln(2*x3**3)
+        assert pytest.approx(3 / 4) == result.dual
