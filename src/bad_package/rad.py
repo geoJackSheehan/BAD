2386f4eec481379a87294e27c8fef27b9439c201
Annabel Yim <annabelyim@g.harvard.edu>
2022-11-29 00:46:30 -0500

    getting started with reverse mode

---
+++ src/bad_package/rad.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+class ReverseMode:
+    _supported_scalars = (int, float)
+
+    def __init__(self, real):
+        self.real = real
+        self.child = []
+        self.grad = None
+
+    def __add__(self, other):
+        if not isinstance(other, (*self._supported_scalars, ReverseMode)):
+            raise TypeError("Type not supported: must be int or float")
+        if isinstance(other, self._supported_scalars):
+            f = Reverse_mode(self.real + other)
+            self.child.append((1.0, f))
+        else:
+            f = Reverse_Mode(self.real + other.real)
+            other.child.append((1.0, f))
+            self.child.append((1.0, f))
+        return f
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        if not isinstance(other, (*self._supported_scalars, ReverseMode)):
+            raise TypeError("Type not supported: must be int or float")
+        if isinstance(other, self._supported_scalars):
+            f = Reverse_mode(self.real - other)
+            self.child.append((1.0, f))
+        else:
+            f = Reverse_Mode(self.real - other.real)
+            other.child.append((1.0, f))
+            self.child.append((-1.0, f))
+        return f
+
+    def __rsub__(self, other):
+        return self.__sub__(other)
