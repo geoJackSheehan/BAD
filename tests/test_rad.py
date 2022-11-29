874eddf7027d1b2720f665553d1b159e030ac0aa
Hope Neveux <hopeneveux@g.harvard.edu>
2022-11-29 15:44:35 -0500

    Shell for rad interface

---
+++ tests/test_rad.py
@@ -0,0 +1,21 @@
+import pytest
+from bad_package.rad import ReverseMode
+import numpy as np
+
+class TestReverseMode:
+
+    def test_init(self):
+        raise NotImplementedError
+
+    def test_add(self):
+        raise NotImplementedError
+
+    def test_radd(self):
+        raise NotImplementedError
+
+    def test_sub(self):
+        raise NotImplementedError
+
+    def test_rsub(self):
+        raise NotImplementedError
+
