3334eea4336e64aab7b1fc0c7956f64054a21673
Hope Neveux <hopeneveux@g.harvard.edu>
2022-11-29 23:34:26 -0500

    Worked but also doesn't pass just over 90

--- tests/check_coverage.sh
+++ tests/check_coverage.sh
@@ -1,27 +1,5 @@
 #!/usr/bin/env bash
 
-# tool='coverage'
-
-# if [[ $# -gt 0 ]]; then
-#     # optional argument to use different tool to check coverage
-#     tool="${1}"; shift
-# fi
-
-# if [[ ${tool} == 'coverage' ]]; then
-#     # run the tests (generates coverage data to build report)
-#     ./run_tests.sh coverage run --source=bad_package "${@}"
-
-#     # build the coverage report on stdout
-#     coverage report -m
-# elif [[ ${tool} == 'pytest' ]]; then
-#     # generate coverage reports with pytest in one go
-#     ./run_tests.sh pytest --cov=bad_package --cov-fail-under=90"${@}"
-# else
-#     # error: write to stderr
-#     >&2 echo "Error: unknown tool '${tool}'"
-#     exit 1
-# fi
-
 export PYTHONPATH="$(pwd -P)/../src":${PYTHONPATH}
 
 pytest --cov=bad_package --cov-fail-under=90
