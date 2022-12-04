9e1a56a40b72ff7df9694f85dabccbfab132b84e
Jack Sheehan <jacksheehan@g.harvard.edu>
2022-12-04 15:39:09 -0500

    altered test_grad() to reflect changes to grad

--- tests/test_rad.py
+++ tests/test_rad.py
@@ -10,7 +10,7 @@ class TestReverseMode:
         
         assert rm.real == 3
         assert len(rm.child) == 0
-        assert rm.grad_value == None
+        assert rm.gradient == None
 
         with pytest.raises(TypeError):
             ReverseMode('a')
@@ -19,11 +19,18 @@ class TestReverseMode:
             ReverseMode(DualNumber(1))
             ReverseMode([])
 
+#     def test_grad(self):
+#         rm = ReverseMode(3)
+#         res1 = rm**2
+#         res1.gradient = 1.0
+#         assert rm.grad() == sum(weight * var.grad() for weight, var in rm.child)
+#         assert rm.grad() == 6
+        
     def test_grad(self):
         rm = ReverseMode(3)
         res1 = rm**2
-        res1.grad_value = 1.0
-        assert rm.grad() == sum(weight * var.grad() for weight, var in rm.child)
+        res1.gradient = 1.0
+        assert rm.grad() == sum(dvj_dvi * df_dvj.grad() for dvj_dvi, df_dvj in rm.child)
         assert rm.grad() == 6
     
     def test_child(self):
